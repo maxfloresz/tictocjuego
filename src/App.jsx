afc61b2844719e71abbdcbf18d6428b01e13d3c4
maxfloresz <108137047+maxfloresz@users.noreply.github.com>
2024-07-07 14:08:08 -0500

    El juego se detiene cuando tiene un ganador

--- src/App.jsx
+++ src/App.jsx
@@ -6,13 +6,37 @@ const TURNOS = {
   X: "x",
   O: "o",
 };
+const WINNER_COMBOS = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6],
+]
 
 function App() {
   const [board, setBoard] = useState(Array(9).fill(null));
   const [turno, setTurno] = useState(TURNOS.X);
   const [winner, setWinner] = useState(null);
+
+  const checkWinner=(boardToCheck)=>{
+    for (const combo of WINNER_COMBOS) {
+      const [a, b, c] = combo;
+      if(
+        boardToCheck[a] &&
+        boardToCheck[a] === boardToCheck[b] &&
+        boardToCheck[a] === boardToCheck[c]
+      ){
+        return boardToCheck[a]
+      }
+    }
+    return null;
+  }
   const updataBoard = (index)=>{
-    if (board[index]) {
+    if (board[index] || winner) {
       return
     }
     const newBoard = [...board];
@@ -20,6 +44,11 @@ function App() {
     setBoard(newBoard);
     const newTurno = turno === TURNOS.X ? TURNOS.O : TURNOS.X;
     setTurno(newTurno);
+    // revisar si hay ganador
+    const newWinner = checkWinner(newBoard);
+    if (newWinner) {
+      setWinner(newWinner)
+    }
   }
   return (
     <>
